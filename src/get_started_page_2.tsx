919baa8f20c07cb2e3531fa17f1292ec7685b0c6
Yewo Manombo <ymanombo@gmail.com>
2025-05-08 20:29:06 +0200

    minor changes on user input validation

--- src/get_started_page_2.tsx
+++ src/get_started_page_2.tsx
@@ -7,30 +7,50 @@ interface IFormInput {
 }
 
 export default function GetStartedPage2() {
-    console.log(localStorage.getItem("user_data"));
-    const { register, handleSubmit, watch, formState: {isValid} } = useForm<IFormInput>();
-    const watchField = watch(["password", "confirm_password"]);
-    console.log(isValid);
+    const { register, handleSubmit, watch, formState: {isValid}, formState: {errors} } = useForm<IFormInput>();
+    const watchField = watch(["password", "confirm_password"], { password: " ", confirm_password: " " });
+
     const onSubmit: SubmitHandler<IFormInput> = (data) => {
-        let user_data = JSON.parse(localStorage.getItem("user_data") || "{}");
+        // get the user data from session storage
+        // and add the password to it
+        let user_data = JSON.parse(sessionStorage.getItem("user_data") || "{}");
         user_data.password = data.password
-        console.log(user_data);
+
+        // make a POST request to the API endpoint with the form data
+        const response = fetch("http://localhost:8000/trail-marker-accounts/users/",{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(user_data)}
+        )
+        response.then((res) => {
+            if (res.ok) { alert("User created successfully") }
+            else {
+                res.text().then((text) => {
+                    console.log(text)
+                })
+            }
+        })
+        .catch((error) => {
+            alert("Error creating user: " + error.message)
+        })
     }
+
     return (
         <div className="flex flex-col justify-center items-center font-grotesk h-screen bg-gradient-to-b from-[#F5F5F5] to-[#FFFFFF] gap-4 p-2">
             <h1 className="text-3xl text-purple-600 font-black">Get started</h1>
 
             <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-2 mb-4 w-1/2 md:w-auto items-center text-center">
                 <input
-                    {...register("password", { required: {value: true, message: "Password is required"},
-                        minLength: {value: 8, message:"Passoword must be atleast 8 characters"} })}
+                    {...register("password", { required: true, minLength: {value: 8, message: "Password must be at least 8 characters" } })}
                     className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring focus:ring-purple-500
                     placeholder:text-gray-400 placeholder:font-light placeholder:text-sm"
                     placeholder="password"
                     type = "password"
                 />
-                {(watchField[0].length < 7) ? 
-                <p className="text-red-500 text-sm"> &#10060; Password must have atleast 8 characters</p> : <p className="text-green-500 text-sm"> &#10004; Password is valid</p>}
+                {errors.password && <p className="text-red-500 text-sm"> &#10060; {errors.password.message}</p>}
+                <p className="text-gray-500 text-sm">Password must be at least 8 characters</p>
                 <input
                     {...register("confirm_password", { required: true, minLength: 8, validate: (value) => value === watchField[0] || "Passwords do not match" })}
                     className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring focus:ring-purple-500   
@@ -38,9 +58,8 @@ export default function GetStartedPage2() {
                     placeholder="confirm password"
                     type = "password"
                 />
-                {(watchField[0] !== watchField[1]) ? 
-                <p className="text-red-500 text-sm"> &#10060; Passwords do not match</p> : <p className="text-green-500 text-sm"> &#10004; Passwords match</p>}
-                <Button className="w-30 text-base bg-purple-600" type="submit" disabled={isValid}>Proceed</Button>
+                {errors.confirm_password && <p className="text-red-500 text-sm"> &#10060; {errors.confirm_password.message}</p>}
+                <Button className="w-30 text-base bg-purple-600" type="submit">Proceed</Button>
             </form>
         </div>
     );
