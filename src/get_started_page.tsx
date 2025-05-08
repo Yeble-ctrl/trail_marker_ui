89363b80fdc4519fcba25a7255d2624ae3430b36
Yewo Manombo <ymanombo@gmail.com>
2025-05-08 17:46:09 +0200

    add validation for unique username

--- src/get_started_page.tsx
+++ src/get_started_page.tsx
@@ -1,6 +1,7 @@
 import { useForm, SubmitHandler } from "react-hook-form";
 import { Button } from "@/components/ui/button";
 import { Link, useNavigate } from 'react-router-dom'
+import { useState, useEffect } from "react";
 
 interface IFormInput {
     first_name: string;
@@ -18,27 +19,29 @@ export interface FormValues {
 }
 
 export default function GetStartedPage() {
-    const navigate = useNavigate();
-    const { register, handleSubmit } = useForm<IFormInput>();
-    const onSubmit: SubmitHandler<IFormInput> = (data) => {
-        navigate("/get_started_page_2");
-        console.log(data);
-        localStorage.setItem("user_data", JSON.stringify(data));
-        /*console.log(data);
-        // Send a POST request to the API endpoint with the form data
-        const response = fetch("http://localhost:8000/trail-marker-accounts/users/",{
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(data)}
-        )
-        response.then((res) => {
-            alert(`${res.status}: ${res.statusText}}`)
+    const [usernames, setUsernames] = useState<String[]>(['']);
+    useEffect(() => {
+        const fetchUsers = fetch("http://localhost:8000/trail-marker-accounts/users/", {method: "GET"})
+        fetchUsers.then((res) => {
+            if (res.ok) {
+                res.json().then((data) => {
+                    let usernames: string[] = [];
+                    data.map((user: IFormInput) => usernames.push(user.username));
+                    setUsernames(usernames);
+                })
+            } else {
+                res.text().then((text) => {console.log(text)})
+            }
         })
         .catch((error) => {
-            alert("Error creating user: " + error.message)
-        })*/
+            alert("Error fetching users: " + error.message)
+        })
+    }, [])
+    const navigate = useNavigate();
+    const { register, handleSubmit, formState: {errors} } = useForm<IFormInput>();
+    const onSubmit: SubmitHandler<IFormInput> = (data) => {
+        navigate("/get_started_page_2");
+        sessionStorage.setItem("user_data", JSON.stringify(data));
     }
     return (
         <div className="flex flex-col justify-center items-center font-grotesk h-screen bg-gradient-to-b from-[#F5F5F5] to-[#FFFFFF] gap-4 p-2">
@@ -60,7 +63,14 @@ export default function GetStartedPage() {
                     type="text"
                 />
                 <input
-                    {...register("username", { required: true, minLength: 4 })}
+                    {...register("username", { required: true, minLength: {value:4, message:'Username cannot be less than 4 characters'},
+                        validate: (value) => {
+                            if (usernames.includes(value)) {
+                                return "Username already exists";
+                            } else {
+                                return true;
+                            }
+                        }})}
                     className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring focus:ring-purple-500
                     placeholder:text-gray-400 placeholder:font-light placeholder:text-sm"
                     placeholder="username"
@@ -73,6 +83,7 @@ export default function GetStartedPage() {
                     placeholder="email address"
                     type="email"
                 />
+                {errors.username && <p className="text-red-500 text-sm">{errors.username.message}</p>}
                 <Button className="w-30 text-base bg-purple-600" type="submit">Proceed</Button>
             </form>
         </div>
