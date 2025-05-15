f991630415ba581a67336acd96937547c39249e7
Yewo Manombo <ymanombo@gmail.com>
2025-05-15 21:01:04 +0200

    added labels for the input fields to improve readability

--- src/setup_profile_page.tsx
+++ src/setup_profile_page.tsx
@@ -18,9 +18,10 @@ export default function SetUpProfilePage(){
     return(
         <div className="flex flex-col justify-center items-center font-grotesk h-screen bg-gradient-to-b from-[#F5F5F5] to-[#FFFFFF] gap-2 p-2">
             <h1 className="text-3xl text-purple-600 font-black">Trail marker</h1>
-            <h1 className="text-sm text-gray-700 font-normal">Enter your login details</h1>
-            <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-2 mb-4 md:w-auto items-center text-center">
+            <h1 className="text-base text-gray-800 font-normal">Set up your profile details</h1>
+            <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-2 mb-4 md:w-auto text-center">
                 {/* date of birth field */}
+                <label htmlFor="date_of_birth" className="text-sm text-gray-600">Set your date of birth</label>
                 <FormInput
                     register={register} 
                     name="date_of_birth" 
@@ -30,6 +31,7 @@ export default function SetUpProfilePage(){
                 />
 
                 {/* gender field */}
+                <label htmlFor="gender" className="text-sm text-gray-600">Select your gender</label>
                 <select {...register('gender', { required: {value: true, message: 'Please select your gender'} })} 
                 className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring focus:ring-purple-500 placeholder:text-gray-400 placeholder:font-light placeholder:text-sm">
                     <option value="male">male</option>
