5218bc3cfd3820b7d6607ecb0e662a8716c21fc8
Yewo Manombo <ymanombo@gmail.com>
2025-05-16 12:57:48 +0200

    made the input file button more colorful

--- src/setup_profile_page_2.tsx
+++ src/setup_profile_page_2.tsx
@@ -1,6 +1,7 @@
 import { Button } from "./components/ui/button";
 import { Link, useNavigate } from 'react-router-dom';
 import { useForm, SubmitHandler } from "react-hook-form";
+import { useState } from "react";
 
 interface IFormInput {
     user_description: string;
@@ -8,8 +9,8 @@ interface IFormInput {
 }
 
 export default function SetUpProfilePage2() {
+    const[image, setImage] = useState('')
     const navigate = useNavigate();
-    const style = "border border-gray-300 rounded-md p-2 focus:outline-none focus:ring focus:ring-purple-500 placeholder:text-gray-600 placeholder:font-medium placeholder:text-sm";
     const {register, handleSubmit} = useForm<IFormInput>();
     const onsubmit: SubmitHandler<IFormInput> = (data) => {
         let userProfileData = JSON.parse(localStorage.getItem('userProfileData') || '{}');
@@ -41,25 +42,39 @@ export default function SetUpProfilePage2() {
             })
             .catch((error) => {console.log(error.message)})
     }
+
     return (
         <div className="flex flex-col font-grotesk justify-center h-screen items-center bg-gradient-to-b from-[#F5F5F5] to-[#FFFFFF] gap-2">
             <h1 className="text-3xl text-purple-600 font-black">Trail marker</h1>
             <h1 className="text-lg text-gray-800 font-medium">Set up your profile details</h1>
-            <form onSubmit={handleSubmit(onsubmit)} className="flex flex-col gap-2 mb-4 md:w-auto text-center"> 
-                <textarea
-                    {...register('user_description', {required: false})}
-                    className={style}
-                    aria-rowspan={2}
-                    placeholder="Add a short description of yourself..."
-                />
-                <label className="text-center font-medium text-gray-600">Upload profile picture</label>
-                <input
-                    {...register('profile_picture', {required: false})}
-                    type="file"
-                    accept="image/png, image/jpeg"
-                    className="border border-gray-300 rounded-md p-2 w-auto cursor-pointer"
-                    multiple = {false}
-                />
+            <form onSubmit={handleSubmit(onsubmit)} className="flex flex-col gap-2 mb-4 md:w-auto text-center">
+                <label htmlFor="user_description">
+                    <textarea
+                        {...register('user_description', {required: false})}
+                        className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring focus:ring-purple-500 placeholder:text-gray-600 placeholder:font-medium placeholder:text-base"
+                        aria-rowspan={2}
+                        placeholder="Add a short description of yourself..."
+                        id="user_description"
+                    />
+                </label>
+                <label 
+                    className="flex flex-col items-center justify-center text-center h-28 cursor-pointer font-medium text-gray-600 border-2 border-gray-300 border-dashed rounded-md p-2"
+                >
+                    <input
+                        {...register('profile_picture', {required: false})}
+                        type="file"
+                        accept="image/png, image/jpeg"
+                        hidden
+                        onChange={(property) => {
+                            if(property.target.files !== null){
+                                let imageUrl = URL.createObjectURL(property.target.files[0]);
+                                setImage(imageUrl)
+                            }
+                        }}
+                    />                    
+                    {image.length? <img src={image} alt="upload" className="h-28"/> : <img src="/upload_icon.svg" alt="upload" className="w-20 h-20"/> }
+                    {image.length? '' : <p className="font-medium text-base">Upload profile photo</p>}
+                </label>
                 <div className="flex flex-row justify-center items-center pt-4">
                     <Button className="w-30 text-lg bg-purple-600" type="submit">Proceed</Button>
                     <Link to={"/home_page"}>
